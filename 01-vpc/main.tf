e6884424c9d08dd81cb9be610edf075c489c0f83
divya <divya.vegesna55@gmail.com>
2024-05-15 15:48:50 +0530

    finl

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -4,6 +4,7 @@ module "roboshop" {
   Environment = var.Environment
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
+  cidr_block = var.cidr_block
   public_cidr = var.public_subnet_cidr
   private_cidr = var.private_subnet_cidr
   database_cidr = var.database_subnet_cidr
--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -34,3 +34,7 @@ variable "is_peering_required" {
   type = bool
   default = true
 }
+variable "cidr_block" {
+    type = string
+    default = "10.1.0.0/16"
+  }
