9e083d6aef873d663e313bfa33f88b6a1ba97093
divya <divya.vegesna55@gmail.com>
2024-05-15 15:42:17 +0530

    finl

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -4,8 +4,8 @@ module "roboshop" {
   Environment = var.Environment
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
-  public_subnet_cidr = var.public_subnet_cidr
-  private_subnet_cidr = var.private_subnet_cidr
-  database_subnet_cidr = var.database_subnet_cidr
+  public_cidr = var.public_subnet_cidr
+  private_cidr = var.private_subnet_cidr
+  database_cidr = var.database_subnet_cidr
   is_peering_required = var.is_peering_required
 }
